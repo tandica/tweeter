7ecf42eff0761d9571ced21d78438ad67570b7dd
Tandica <williams.tandica@gmail.com>
2021-05-19 17:17:56 +0000

    Added profile icon in tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,20 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// const tweetData = {
+//     "user": {
+//     "name": "Newton",
+//     "avatars": "https://i.imgur.com/73hZDYK.png",
+//     "handle": "@SirIsaac"
+//     },
+//     "content": {
+//     "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+// }
+
+// const $tweet = $(`<article class="tweet">Hello world</article>`);
+
+// const createTweetElement = function (tweet) {
+//     let $tweet = 
+// }
