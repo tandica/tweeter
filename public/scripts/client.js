555310b1fb786705f5d5a41c901c5fa7659459e0
Tandica <williams.tandica@gmail.com>
2021-05-19 19:21:46 +0000

    created new tweets from data and updated css

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,18 +6,75 @@
 
 // const tweetData = {
 //     "user": {
-//     "name": "Newton",
-//     "avatars": "https://i.imgur.com/73hZDYK.png",
-//     "handle": "@SirIsaac"
+//         "name": "Newton",
+//         "avatars": "https://i.imgur.com/73hZDYK.png",
+//         "handle": "@SirIsaac"
 //     },
 //     "content": {
-//     "text": "If I have seen further it is by standing on the shoulders of giants"
+//         "text": "If I have seen further it is by standing on the shoulders of giants"
 //     },
 //     "created_at": 1461116232227
 // }
 
-// const $tweet = $(`<article class="tweet">Hello world</article>`);
+//const $tweet = $(`<article class="tweet">Hello world</article>`);
 
-// const createTweetElement = function (tweet) {
-//     let $tweet = 
-// }
+const data = [{
+    "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+    },
+    "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+    },
+    {
+    "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+    "content": {
+        "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+    }
+]
+
+
+
+$(document).ready(function() {
+    const renderTweets = function(tweets) {
+        for (let tweet of tweets) {
+            let $tweet = createTweetElement(tweet);
+            console.log($tweet);
+            $('.tweets-container').append($tweet);
+            console.log($('.tweets-container'))
+        }
+    }
+    renderTweets(data);
+    
+})
+
+
+const createTweetElement = function (tweet) {
+    let $tweet = ` 
+    <article class="tweet-container">
+        <h5>${tweet["user"]["handle"]}</h5>
+        <img src=${tweet["user"]["avatars"]}> 
+        <h4>${tweet["user"]["name"]}</h4>
+            <p>${tweet["content"]["text"]}</p>
+        <div class = "outer-border"></div>
+        <div class = "inner-border"></div>
+        <time class="timeago" datetime="2021-05-18T09:24:17Z" title="May 18, 2021"></time>
+        <i class="fas fa-heart"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-flag"></i>
+    </article>`;
+
+    return $tweet;
+}
+
+
+//const $tweet = createTweetElement(tweetData);
+//console.log($tweet); 
